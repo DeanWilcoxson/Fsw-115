ca227d4eedef833d252b104111f89aa68baae145
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-01-14 01:53:21 -0800

    1/13/21

---
+++ week4/Assignment2/Index.js
@@ -0,0 +1,24 @@
+const getData = document.getElementById("SwApi2");
+getData.addEventListener("click", function() {
+    axios.get("https://swapi.dev/api/people")
+        .then(response => {
+            for (i = 0; i < response.data.results.length; i++) {
+                const h1 = document.createElement("h1");
+                h1.innerHTML = `${response.data.results[i].name}`
+                document.body.append(h1)
+            }
+        })
+        .catch((error) => alert("error"))
+})
+const otherData = document.getElementById("SwApi1");
+otherData.addEventListener("click", function() {
+    axios.get("https://swapi.dev/api/planets")
+        .then(response => {
+            for (i = 0; i < response.data.results.length; i++) {
+                const h1 = document.createElement("h1");
+                h1.innerHTML = `${response.data.results[i]}`
+                document.body.append(h1)
+            }
+        })
+        .catch((error) => alert("error"))
+})
