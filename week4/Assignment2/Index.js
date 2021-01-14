13b78a898e31541c4afbf7f14219836a9ca9b9f0
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-01-14 06:12:51 -0800

    1/14/21

--- week4/Assignment2/Index.js
+++ week4/Assignment2/Index.js
@@ -5,9 +5,10 @@ function people() {
                 const h1 = document.createElement("h1");
                 h1.innerHTML = `${response.data.results[i].name}`;
                 h1.style.textAlign = "center";
-                h1.style.color = "blue"
-                h1.style.backgroundColor = "darkgrey"
-                document.body.append(h1);
+                h1.style.color = "blue";
+                h1.style.backgroundColor = "darkgrey";
+                let data = document.getElementById("listData");
+                data.append(h1);
             }
         })
         .catch((error) => alert("error"));
@@ -22,7 +23,8 @@ function planets() {
                 h1.style.textAlign = "center"
                 h1.style.color = "blue"
                 h1.style.backgroundColor = "darkgrey"
-                document.body.append(h1)
+                let data = document.getElementById("listData");
+                data.append(h1);
             }
         })
         .catch((error) => alert("error"))
