0b632a543aca52ecea8c1459f928d63774d99f5e
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-01-27 20:08:23 -0800

    1/27/21

--- week6/index.js
+++ week6/index.js
@@ -12,55 +12,57 @@ const getData = async () => {
       name.className = "name"
       div.className = "item";
       div.append(name)
-      listData.appendChild(div);
+      // listData.appendChild(div);
 
       //Films List Construction
       const filmsArray = person.films;
       if (filmsArray.length) {
-        let ol = document.createElement("ol");
-        ol.innerHTML = "Films";
-        ol.className = "filmsList";
-        div.appendChild(ol);
+        let filmsList = document.createElement("ol");
+        filmsList.innerHTML = "Films";
+        filmsList.className = "filmsList";
         for (var j = 0; j < filmsArray.length; j++) {
           const film = await axios.get(filmsArray[j]);
           const li = document.createElement("li");
           li.textContent = film.data.title;
           li.className = "films";
-          ol.appendChild(li);
+          filmsList.appendChild(li);
         }
+        div.appendChild(filmsList);
       }
 
       //Vehicles List Construction
       const vehiclesArray = person.vehicles;
       if (vehiclesArray.length) {
-        let ol = document.createElement("ol");
-        ol.innerHTML = "Vehicles";
-        ol.className = "vehiclesList";
-        div.appendChild(ol);
+        let vehiclesList = document.createElement("ol");
+        vehiclesList.innerHTML = "Vehicles";
+        vehiclesList.className = "vehiclesList";
         for (var l = 0; l < vehiclesArray.length; l++) {
           const vehicle = await axios.get(vehiclesArray[l]);
           const li = document.createElement("li");
           li.textContent = vehicle.data.name;
           li.className = "vehicles";
-          ol.appendChild(li);
+          vehiclesList.appendChild(li);
         }
+        div.appendChild(vehiclesList);
       }
 
       //Starships List Construction
       const starShipsArray = person.starships;
       if (starShipsArray.length) {
-        let ol = document.createElement("ol");
-        ol.textContent = "Starships";
-        ol.className = "starshipsList";
-        div.appendChild(ol);
+        let starshipsList = document.createElement("ol");
+        starshipsList.textContent = "Starships";
+        starshipsList.className = "starshipsList";
         for (var f = 0; f < starShipsArray.length; f++) {
           const starShip = await axios.get(starShipsArray[f]);
           const li = document.createElement("li");
           li.textContent = starShip.data.name;
           li.className = "starships";
-          ol.appendChild(li);
+          starshipsList.appendChild(li);
         }
+        div.appendChild(starshipsList);
       }
+      
+      listData.appendChild(div);
     }
   } catch (error) {
     console.log("error");
